aef6111f2c8149ff5aeaa5b126bdc9efaeb91039
mr-mohit <44066175+mr-mohit@users.noreply.github.com>
2022-08-04 17:48:13 -0400

    Initialized Eureka Server

--- src/main/java/com/example/demo/MohitJaiAnmolComp303Assignment4Application.java
+++ src/main/java/com/example/demo/MohitJaiAnmolComp303Assignment4Application.java
@@ -2,12 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class MohitJaiAnmolComp303Assignment4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MohitJaiAnmolComp303Assignment4Application.class, args);
+		System.out.println("Discovery Server running.");
 	}
 
 }
